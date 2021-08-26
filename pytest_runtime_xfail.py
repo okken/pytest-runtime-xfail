08a47efb4c00c39819073ecffac8053ea0f2c7d4
Brian Okken <1568356+okken@users.noreply.github.com>
2021-08-26 11:08:47 -0700

    doc update

--- pytest_runtime_xfail.py
+++ pytest_runtime_xfail.py
@@ -1,9 +1,9 @@
 """
-pytest plugin, providing a `runtime_xfail` fixture, which is callable as `runtime_xfail()`, to allow runtime decisions to mark a test as `xfail`.
+Call runtime_xfail() to mark running test as xfail.
 """
 import pytest
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 
 @pytest.fixture()
